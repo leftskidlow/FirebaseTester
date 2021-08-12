3c123ce8d6de627a810fb117d4a9850b10d9557e
leftskidlow <32857018+leftskidlow@users.noreply.github.com>
2021-08-12 12:00:29 -0400

    Initial Commit

---
+++ AuthTester/AuthTesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthTesterApp.swift
+//  AuthTester
+//
+//  Created by Tim Miller on 8/12/21.
+//
+
+import SwiftUI
+
+@main
+struct AuthTesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
